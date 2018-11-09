e473afad3f789b2660c51a88411e237b5c811040
Mattias Wennmo <wennmo@users.noreply.github.com>
2018-11-09 15:29:17 +0100

    Add command for reloading data in session app and opening catwalk (#1)
    * Add function for reloading script in a sessions app
    
    * Add command for opening catwalk

--- src/validate-model-command.js
+++ src/validate-model-command.js
@@ -1,10 +1,10 @@
-const { window, commands, ViewColumn } = require('vscode');
+const { window, commands, Uri } = require('vscode');
+const { reloadScriptSessionApp} = require("./communication")
 
 exports.validateModelCommand = commands.registerCommand('extension.validateModel', async function () {
-  const panel = window.createWebviewPanel(
-    'catwalk', // Identifies the type of the webview. Used internally
-    "catwalk", // Title of the panel displayed to the user
-    ViewColumn.One, // Editor column to show the new webview panel in.
-    {} // Webview options. More on these later.
-  );
+  const editor = window.activeTextEditor;
+  const script = editor._documentData._lines.join('\r\n');
+  const engineUrl = await reloadScriptSessionApp(script);
+  const catwalkUrl = `https://catwalk.core.qlik.com/?engine_url=${engineUrl}`
+  commands.executeCommand("vscode.open", Uri.parse(catwalkUrl));
 });
