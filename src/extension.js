e7babea62daba73bd33f3872c569d9eb131e4cb4
Mattias Wennmo <mow@qlik.com>
2018-11-09 12:25:30 +0100

    Handle dialogs in checkscriptsyntax

--- src/extension.js
+++ src/extension.js
@@ -6,46 +6,51 @@ const net = require("net");
 const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/12.170.2.json');
 
-const conf = vscode.workspace.getConfiguration("engine");
-const host = conf.get("host");
-const port = conf.get("port");
-
-const engine = `${host}:${port}`;
-console.log(`Using engine: ${engine}`);
-
-const session = enigma.create({
-  schema,
-  url: `ws://${host}:${port}/app/engineData/`,
-  createSocket: url => new WebSocket(url),
-});
-
 async function checkScriptSyntax(script) {
-  //session.on("traffic:*", (direction, data) => console.log("session", direction, JSON.stringify(data)));
-  const qix = await session.open();
-  const app = await qix.createSessionApp();
-  await app.setScript(script);
-  const errors = await app.checkScriptSyntax();
-  console.log(`Errors: ${JSON.stringify(errors)}`);
-  session.close();
-  return errors;
+  const conf = vscode.workspace.getConfiguration("engine");
+  const host = conf.get("host");
+  const port = conf.get("port");
+
+  console.log(`Using engine: ${host}:${port}`);
+
+  const alive = await isReachable(port, { host });
+  if (alive === true) {
+
+    const session = enigma.create({
+      schema,
+      url: `ws://${host}:${port}/app/engineData/`,
+      createSocket: url => new WebSocket(url),
+    });
+
+    //session.on("traffic:*", (direction, data) => console.log("session", direction, JSON.stringify(data)));
+    const qix = await session.open();
+    const app = await qix.createSessionApp();
+    await app.setScript(script);
+    const errors = await app.checkScriptSyntax();
+    console.log(`Errors: ${JSON.stringify(errors)}`);
+    session.close();
+    vscode.window.showInformationMessage(`Found errors: ${JSON.stringify(errors)}`);
+    return errors;
+  }
+  return [];
 }
 
 function isReachable(port, opts) {
   opts = Object.assign({ timeout: 1000 }, opts);
   return new Promise((resolve => {
-      const socket = new net.Socket();
-      const onError = (e) => {
-          socket.destroy();
-          vscode.window.showErrorMessage('Unable to connect to the Qlik Associative Engine.');
-          resolve(false);
-      };
-      socket.setTimeout(opts.timeout);
-      socket.on('error', onError);
-      socket.on('timeout', onError);
-      socket.connect(port, opts.host, () => {
-          socket.end();
-          resolve(true);
-      });
+    const socket = new net.Socket();
+    const onError = (e) => {
+      socket.destroy();
+      vscode.window.showErrorMessage('Unable to connect to the Qlik Associative Engine.');
+      resolve(false);
+    };
+    socket.setTimeout(opts.timeout);
+    socket.on('error', onError);
+    socket.on('timeout', onError);
+    socket.connect(port, opts.host, () => {
+      socket.end();
+      resolve(true);
+    });
   }));
 }
 
@@ -53,25 +58,21 @@ function isReachable(port, opts) {
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.checkScriptSyntax', async function () {
-        // The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('extension.checkScriptSyntax', async function () {
+    // The code you place here will be executed every time your command is executed
 
-        const editor = vscode.window.activeTextEditor;
-        const script = editor._documentData._lines.join('\r\n');
+    const editor = vscode.window.activeTextEditor;
+    const script = editor._documentData._lines.join('\r\n');
 
-        const alive = await isReachable(port, {host});
-        if (alive === true) {
-          // Display a message box to the user
-          vscode.window.showInformationMessage('Lets check the script');
-          const errors = await checkScriptSyntax(script);
-          vscode.window.showInformationMessage(`Found errors: ${JSON.stringify(errors)}`);
-        }
-    });
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Lets check the script');
+    const errors = await checkScriptSyntax(script);
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
