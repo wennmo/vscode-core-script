0ecc0d787dc6db65d682f631579e0a025a53352c
Håkan Axelsson <han@qlik.com>
2018-11-09 10:58:09 +0100

    Use active doc

--- src/extension.js
+++ src/extension.js
@@ -5,44 +5,15 @@ const WebSocket = require('ws');
 const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/12.170.2.json');
 
-const engine = "localhost:19076";
+const engine = "localhost:9076";
 const session = enigma.create({
   schema,
   url: `ws://${engine}/app/engineData/`,
   createSocket: url => new WebSocket(url),
 });
 
-const script = `
-Characters
-Load Chr(RecNo()+Ord('A')-1) as Alpha, RecNo() as Num autogenerate 26;
-ASCII:
-Load
-if(RecNo()>=65 and RecNo()<=90,RecNo()-64) as Num,
-  Chr(RecNo()) as AsciiAlpha,
-ReNo() as AsciiNum
-autogenerate 255
-Where (RecNo()>=32 and RecNo()<=126) or RecNo()>=160 ;
-Transactions:
-Load
-TransLineID,
-TransID,
-mod(TransID,26)+1 as Num,
-Pick(Ceil(3*Rand1),'A','B','C') as Dim1,
-Pick(Ceil(6*Rand1),'a','b','c','d','e','f') as Dim2,
-Pick(Ceil(3*Rand()),'X','Y','Z') as Dim3,
-Round(1000*Rand()*Rand()*Rand1) as Expression1,
-Round(  10*Rand()*Rand()*Rand1) as Expression2,
-Round(Rand()*Rand1,0.00001) as Expression3;
-Load
-Rand() as Rand1,
-IterNo() as TransLineID,
-RecNo() as TransID
-Autogenerate 1000
-While Rand()<=0.5 or IterNo()=1;
-Comment Field Dim1 With "This is a field comment";
-`;
-
-async function checkScriptSyntax() {
+async function checkScriptSyntax(script) {
+  console.log('inside', script);
   //session.on("traffic:*", (direction, data) => console.log("session", direction, JSON.stringify(data)));
   const qix = await session.open();
   const app = await qix.createSessionApp();
@@ -62,9 +33,12 @@ function activate(context) {
     let disposable = vscode.commands.registerCommand('extension.checkScriptSyntax', async function () {
         // The code you place here will be executed every time your command is executed
 
+        const editor = vscode.window.activeTextEditor;
+        const script = editor._documentData._lines.join('\r\n');
+
         // Display a message box to the user
         vscode.window.showInformationMessage('Lets check the script');
-        const errors = await checkScriptSyntax();
+        const errors = await checkScriptSyntax(script);
         vscode.window.showInformationMessage(`Found errors: ${JSON.stringify(errors)}`);
     });
 
