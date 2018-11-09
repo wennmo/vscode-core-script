296463b330acbad84d1271782fcc84bc3991c3bb
Håkan Axelsson <han@qlik.com>
2018-11-09 09:42:40 +0100

    add checkscriptsyntax

---
+++ src/extension.js
@@ -0,0 +1,45 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const checkScriptSyntax = require('check-script-syntax');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vscode-core-script" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.checkScriptSyntax', function () {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Lets check');
+
+        //get config
+
+        //open socket
+
+        //get script from window
+
+        //create session app
+
+        //setscript
+
+        //check script syntax
+
+        //display status
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
