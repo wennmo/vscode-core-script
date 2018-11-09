89891f38bc0bf97bdcddce4f76bffcdfbe48cef5
Mattias Wennmo <mow@qlik.com>
2018-11-09 13:11:52 +0100

    Break out check-script-syntax to separate file

---
+++ src/check-script-syntax.js
@@ -0,0 +1,51 @@
+const WebSocket = require('ws');
+const net = require("net");
+const { window, workspace } = require('vscode');
+const enigma = require('enigma.js');
+const schema = require('enigma.js/schemas/12.170.2.json');
+
+function isReachable(port, opts) {
+  opts = Object.assign({ timeout: 1000 }, opts);
+  return new Promise((resolve => {
+    const socket = new net.Socket();
+    const onError = (e) => {
+      socket.destroy();
+      window.showErrorMessage('Unable to connect to the Qlik Associative Engine.');
+      resolve(false);
+    };
+    socket.setTimeout(opts.timeout);
+    socket.on('error', onError);
+    socket.on('timeout', onError);
+    socket.connect(port, opts.host, () => {
+      socket.end();
+      resolve(true);
+    });
+  }));
+}
+
+exports.checkScriptSyntax = async function checkScriptSyntax(script) {
+  const conf = workspace.getConfiguration("engine");
+  const host = conf.get("host");
+  const port = conf.get("port");
+
+  const alive = await isReachable(port, { host });
+  if (alive === true) {
+
+    const session = enigma.create({
+      schema,
+      url: `ws://${host}:${port}/app/engineData/`,
+      createSocket: url => new WebSocket(url),
+    });
+
+    //session.on("traffic:*", (direction, data) => console.log("session", direction, JSON.stringify(data)));
+    const qix = await session.open();
+    const app = await qix.createSessionApp();
+    await app.setScript(script);
+    const errors = await app.checkScriptSyntax();
+    console.log(`Errors: ${JSON.stringify(errors)}`);
+    session.close();
+    window.showInformationMessage(`Found errors: ${JSON.stringify(errors)}`);
+    return errors;
+  }
+  return [];
+}
--- src/extension.js
+++ src/extension.js
@@ -1,60 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const { window, workspace, StatusBarAlignment, commands } = require('vscode');
-const WebSocket = require('ws');
-const net = require("net");
-const enigma = require('enigma.js');
-const schema = require('enigma.js/schemas/12.170.2.json');
+const { window, StatusBarAlignment, commands } = require('vscode');
+const { checkScriptSyntax } = require("./check-script-syntax")
 
 let nbrScriptErrors = 0;
-
-async function checkScriptSyntax(script) {
-  const conf = workspace.getConfiguration("engine");
-  const host = conf.get("host");
-  const port = conf.get("port");
-
-  console.log(`Using engine: ${host}:${port}`);
-
-  const alive = await isReachable(port, { host });
-  if (alive === true) {
-
-    const session = enigma.create({
-      schema,
-      url: `ws://${host}:${port}/app/engineData/`,
-      createSocket: url => new WebSocket(url),
-    });
-
-    //session.on("traffic:*", (direction, data) => console.log("session", direction, JSON.stringify(data)));
-    const qix = await session.open();
-    const app = await qix.createSessionApp();
-    await app.setScript(script);
-    const errors = await app.checkScriptSyntax();
-    console.log(`Errors: ${JSON.stringify(errors)}`);
-    session.close();
-    window.showInformationMessage(`Found errors: ${JSON.stringify(errors)}`);
-    return errors;
-  }
-  return [];
-}
-
-function isReachable(port, opts) {
-  opts = Object.assign({ timeout: 1000 }, opts);
-  return new Promise((resolve => {
-    const socket = new net.Socket();
-    const onError = (e) => {
-      socket.destroy();
-      window.showErrorMessage('Unable to connect to the Qlik Associative Engine.');
-      resolve(false);
-    };
-    socket.setTimeout(opts.timeout);
-    socket.on('error', onError);
-    socket.on('timeout', onError);
-    socket.connect(port, opts.host, () => {
-      socket.end();
-      resolve(true);
-    });
-  }));
-}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
